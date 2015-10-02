f9a8dbf70e9d3553e02d5259933ed2478533db5f
Dzhemal Orazgeldyeva <Dzhemal_Orazgeldyeva@epam.com>
2015-10-02 14:59:57 +0300

    Done the JS task

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,25 @@
-export { memoize };
+export {
+	memoize
+};
+
+
+
 
 function memoize(func) {
-    return func; // TODO
+	var cache = {};
+	var checkCache = function() {
+		var index = "";
+		for (var i = 0; i < arguments.length; i++) {
+			index += " " + arguments[i] + "-" + {}.toString.call(arguments[i]);
+		}
+		if (index in cache) {
+			return cache[index];
+
+		} else {
+			cache[index] = func.apply(this, arguments);
+			return cache[index];
+		}
+
+	};
+	return checkCache;
 }
